e9b7b71ab8fe6246525ccad14acfe80a4f79b12b
hmathlee <h349lee@uwaterloo.ca>
2024-03-21 03:18:54 -0400

    Cleaned up main loop

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import pandas as pd
-from utils import data_split
-from models import XGBoost, Logistic
+from utils import read_csv_data, df_copy, preprocess
+from models import XGBoost, Logistic, SVM, NeuralNetwork
 
+# Paths
 X_TRAIN_PATH = "data/X_train.csv"
 Y_TRAIN_PATH = "data/y_train.csv"
-
 X_TEST_PATH = "data/X_test.csv"
 
+# Constants
 VAL_SIZE = 0.2
 RANDOM_STATE = 420
 
+# Driver code
 if __name__ == "__main__":
     # 80% of the data is training; to get (VAL_SIZE * 100)% validation data, divide by 0.8
     val_sample = VAL_SIZE / 0.8
-    datasets = data_split(X_TRAIN_PATH, Y_TRAIN_PATH, X_TEST_PATH, val_sample, RANDOM_STATE)
+
+    dfs = read_csv_data(X_TRAIN_PATH, Y_TRAIN_PATH, X_TEST_PATH, val_sample)
+
+    # Deep copy the dataframes so that the originals remain intact after preprocessing
+    X_train, y_train, X_test, val_idx = df_copy(dfs)
+    dataset = preprocess(X_train, y_train, X_test, val_idx)
 
     val_metrics = {}
 
-    models = [Logistic(), XGBoost()]
-    enc = True
+    # models = [Logistic(), XGBoost(), SVM(), NeuralNetwork()]
+    models = [XGBoost()]
     for model in models:
-        # Dynamically generate the required version of the dataset: reduce runtime
-        # Generate the one-hot version of the datasets
-        if model.needs_one_hot and not enc:
-            datasets = data_split(X_TRAIN_PATH, Y_TRAIN_PATH, X_TEST_PATH, val_sample, RANDOM_STATE, True)
-        # Generate the categorical version of the datasets
-        elif not model.needs_one_hot and enc:
-            datasets = data_split(X_TRAIN_PATH, Y_TRAIN_PATH, X_TEST_PATH, val_sample, RANDOM_STATE, False)
-        enc = model.needs_one_hot
+        # Call model's separate preprocessor function if applicable
+        if model.default_preprocessor():
+            dat = dataset
+        else:
+            X_train, y_train, X_test, val_idx = df_copy(dfs)
+            dat = preprocess(X_train, y_train, X_test, val_idx,
+                             model.one_hot_encode, model.feature_select, model.scale)
 
         # Train each model and report validation metrics
-        X_train, y_train = datasets["train"]
-        X_val, y_val = datasets["val"]
+        X_train, y_train, X_val, y_val, X_test = dat
+        print("+================================+")
+        print("Fitting " + model.model + " with:")
+        print("n_train = " + str(len(X_train)))
+        print("n_val = " + str(len(X_val)))
+        print("Training features = " + str(len(X_train.columns)))
         val_metrics[model.model] = model.train(X_train, y_train, X_val, y_val)
 
     print(val_metrics)
